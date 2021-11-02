38a41331663f99a98c4e20e1370ba0d23ce5c253
alex-1906 <93399025+alex-1906@users.noreply.github.com>
2021-11-02 07:35:18 +0100

    Add files via upload

---
+++ categories.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('movies.csv')
+print(data.columns)
+
+data_genres = pd.pivot_table(data,index=data['title'],columns=data['genres'])
+print(data.loc[0])
+#print(data['genres'].values)
+
+data2 = pd.read_csv('movies_metadata.csv',low_memory=False)
+print(data2['genres'])
